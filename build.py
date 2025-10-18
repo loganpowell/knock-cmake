33bf1c9447044f83835dc5ee4597475af6e5e140
loganpowell <loganp@tepper.cmu.edu>
2025-10-18 11:39:25 -0400

    fixes build.py type errors

--- build.py
+++ build.py
@@ -9,77 +9,115 @@ import shutil
 import subprocess
 from subprocess import run
 from pathlib import Path
+from typing import Optional
 
 # default build config directories
 SOURCE_DIR = Path(__file__).resolve().parent
-BUILD_DIR = f"{SOURCE_DIR}/~build"
-CHECKOUT_DIR = f"{SOURCE_DIR}/~checkout"
-INSTALL_DIR = f"{SOURCE_DIR}/knock"
-libgourou_DIR = f"{CHECKOUT_DIR}/libgourou"
-updfparser_DIR = f"{CHECKOUT_DIR}/uPDFParser"
-knock_DIR = f"{CHECKOUT_DIR}/knock"
+BUILD_DIR = Path(f"{SOURCE_DIR}/~build")
+CHECKOUT_DIR = Path(f"{SOURCE_DIR}/~checkout")
+INSTALL_DIR = Path(f"{SOURCE_DIR}/knock")
+libgourou_DIR = Path(f"{CHECKOUT_DIR}/libgourou")
+updfparser_DIR = Path(f"{CHECKOUT_DIR}/uPDFParser")
+knock_DIR = Path(f"{CHECKOUT_DIR}/knock")
 
-# helper functions 
+
+# helper functions
 def check_binary_dependency(name: str, critical=True) -> bool:
     try:
-        proc = run("git", stdout=subprocess.PIPE, stderr=subprocess.PIPE) # pipe to silence the help messages
+        proc = run(
+            "git", stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )  # pipe to silence the help messages
     except FileNotFoundError:
-        if not critical: return False
-        raise f"This script needs `{name}`, but `{name}` is not found."
+        if not critical:
+            return False
+        raise RuntimeError(f"This script needs `{name}`, but `{name}` is not found.")
     return True
 
-def get_git_repo(repoPath: str, outputDir: Path, tag: str = None, commitHash:str = None) -> bool: 
+
+def get_git_repo(
+    repoPath: str,
+    outputDir: Path,
+    tag: Optional[str] = None,
+    commitHash: Optional[str] = None,
+) -> bool:
     outputDir = Path(outputDir)
-    assert not outputDir.exists(), f"Git clone output set to `{outputDir.resolve()}`, but `{outputDir.resolve()}` already exists."
+    assert (
+        not outputDir.exists()
+    ), f"Git clone output set to `{outputDir.resolve()}`, but `{outputDir.resolve()}` already exists."
     cmd_options = []
-    if (tag != None): cmd_options.extend(["-b", tag])
+    if tag != None:
+        cmd_options.extend(["-b", tag])
     cmd = ["git", "clone"]
     cmd.extend(cmd_options)
     cmd.extend([repoPath, str(outputDir.absolute())])
     proc = run(cmd)
     assert proc.returncode == 0, f"Git Clone `{repoPath}` failed"
-    if (commitHash == None): return True
+    if commitHash == None:
+        return True
     proc = run(["git", "reset", "--hard", commitHash], cwd=str(outputDir.resolve()))
     assert proc.returncode == 0, f"Failed to set repo to targeted commit."
     return True
 
-def cp(sourcePath: Path, targetPath: Path, copyContents:bool = False):
+
+def cp(sourcePath: Path, targetPath: Path, copyContents: bool = False):
     sourcePath = Path(sourcePath).resolve()
     targetPath = Path(targetPath).absolute()
-    if sourcePath.is_file(): shutil.copyfile(sourcePath, targetPath)
-    elif sourcePath.is_dir() and copyContents: shutil.copytree(sourcePath, targetPath, dirs_exist_ok=True)
-    elif sourcePath.is_dir() and not copyContents: 
+    if sourcePath.is_file():
+        shutil.copyfile(sourcePath, targetPath)
+    elif sourcePath.is_dir() and copyContents:
+        shutil.copytree(sourcePath, targetPath, dirs_exist_ok=True)
+    elif sourcePath.is_dir() and not copyContents:
         newTarget = Path(f"{targetPath}/{sourcePath.name}")
-        if not newTarget.exists() : os.mkdir(newTarget)
+        if not newTarget.exists():
+            os.mkdir(newTarget)
         shutil.copytree(sourcePath, newTarget, dirs_exist_ok=True)
 
+
 def rmdir_if_exist(path):
-    if Path(path).exists(): shutil.rmtree(path)
+    if Path(path).exists():
+        shutil.rmtree(path)
+
 
 def clean():
     rmdir_if_exist(CHECKOUT_DIR)
     rmdir_if_exist(BUILD_DIR)
     rmdir_if_exist(INSTALL_DIR)
 
+
 def autoget_apt_pkg():
     if (not check_binary_dependency("apt")) or (not os.geteuid() == 0):
-        print("Auto pkg get not available; Please ensure the following libraries are installed: build-essential, git, cmake, libssl-dev, libcurl4-openssl-dev, zlib1g-dev")
+        print(
+            "Auto pkg get not available; Please ensure the following libraries are installed: build-essential, git, cmake, libssl-dev, libcurl4-openssl-dev, zlib1g-dev"
+        )
         return
-    run(["apt", "install", "build-essential", "git", "cmake", "libssl-dev", "libcurl4-openssl-dev", "zlib1g-dev", "-y"])
+    run(
+        [
+            "apt",
+            "install",
+            "build-essential",
+            "git",
+            "cmake",
+            "libssl-dev",
+            "libcurl4-openssl-dev",
+            "zlib1g-dev",
+            "-y",
+        ]
+    )
+
 
 ###########
 # INSTALL #
 ###########
 # This can be done through a shell if python is not available, just follow the steps and use equivalent commands in shell
 if __name__ == "__main__":
-    #clean repo of old build artifacts
+    # clean repo of old build artifacts
     clean()
 
     # install package manager dependencies if apt is available and script have root perms
     # packages needed: build-essential, git, cmake, libssl-dev, libcurl4-openssl-dev, zlib1g-dev
     autoget_apt_pkg()
 
-    # check if build tools exist 
+    # check if build tools exist
     check_binary_dependency("git")
     check_binary_dependency("cmake")
 
@@ -91,21 +129,47 @@ if __name__ == "__main__":
     # then run `git reset --hard <commit hash>` in each of the repos with their appropriate commit hashes listed here
 
     #############<-repo url---------------------------------------><-output dir-------><-tag-------><-git hash------------------------------->
-    get_git_repo("https://forge.soutade.fr/soutade/libgourou.git",  libgourou_DIR ,     "master",   "81faf1f9bef4d27d8659f2f16b9c65df227ee3d7")
-    get_git_repo("https://forge.soutade.fr/soutade/uPDFParser",     updfparser_DIR ,    "master",   "6060d123441a06df699eb275ae5ffdd50409b8f3")
-    #get_git_repo("https://github.com/BentonEdmondson/knock",        knock_DIR,          "79",       "0aa4005fd4f2ee1b41c20643017c8f0a2bdf6262")
-    get_git_repo("https://github.com/Alvin-He/knock-cmake",        knock_DIR,          "knock-base-release-79",       "0aa4005fd4f2ee1b41c20643017c8f0a2bdf6262")
+    get_git_repo(
+        "https://forge.soutade.fr/soutade/libgourou.git",
+        libgourou_DIR,
+        "master",
+        "81faf1f9bef4d27d8659f2f16b9c65df227ee3d7",
+    )
+    get_git_repo(
+        "https://forge.soutade.fr/soutade/uPDFParser",
+        updfparser_DIR,
+        "master",
+        "6060d123441a06df699eb275ae5ffdd50409b8f3",
+    )
+    # get_git_repo("https://github.com/BentonEdmondson/knock",        knock_DIR,          "79",       "0aa4005fd4f2ee1b41c20643017c8f0a2bdf6262")
+    get_git_repo(
+        "https://github.com/loganpowell/knock-cmake",
+        knock_DIR,
+        "knock-base-release-79",
+        "0aa4005fd4f2ee1b41c20643017c8f0a2bdf6262",
+    )
 
-    # copy the needed build configuration files into those dependencies 
+    # copy the needed build configuration files into those dependencies
     # move each of the CMakeLists.txt and any other files in ./config/<repo name>/ into each of their corresponding folders in ~checkout
-    cp(f"{SOURCE_DIR}/config/libgourou/", libgourou_DIR, True)
-    cp(f"{SOURCE_DIR}/config/uPDFParser", updfparser_DIR, True)
-    cp(f"{SOURCE_DIR}/config/knock", knock_DIR, True)
-    
-    # run cmake configure and build commands 
-    # literally copy all the arguments on each line into a shell and run them 
+    cp(Path(f"{SOURCE_DIR}/config/libgourou/"), libgourou_DIR, True)
+    cp(Path(f"{SOURCE_DIR}/config/uPDFParser"), updfparser_DIR, True)
+    cp(Path(f"{SOURCE_DIR}/config/knock"), knock_DIR, True)
+
+    # run cmake configure and build commands
+    # literally copy all the arguments on each line into a shell and run them
     run(["cmake", "-S", ".", "-B", BUILD_DIR], cwd=SOURCE_DIR)
-    run(["cmake", "--build", BUILD_DIR, "--config", "Release", "-j", str(os.cpu_count())], cwd=SOURCE_DIR)
+    run(
+        [
+            "cmake",
+            "--build",
+            BUILD_DIR,
+            "--config",
+            "Release",
+            "-j",
+            str(os.cpu_count()),
+        ],
+        cwd=SOURCE_DIR,
+    )
     run(["cmake", "--install", BUILD_DIR], cwd=SOURCE_DIR)
-    
+
     print(f"build finished, the knock binary is located in: {INSTALL_DIR}")
